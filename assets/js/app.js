22aea420141a000c545a01a1ba5eacb755a6f4b1
EmilyT <emilython@hotmail.com>
2022-01-18 14:34:11 -0600

    populates questions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,24 @@
 // Questions array
-
+const questions = [
+  {
+    question: 'Commonly used data types DO NOT include:',
+    answers: [
+      { text: 'strings', correct: false },
+      { text: 'booleans', correct: false },
+      { text: 'alerts', correct: true },
+      { text: 'numbers', correct: false },
+    ],
+  },
+];
 // Variables
 const startButton = document.getElementById('start-btn');
 const startPage = document.getElementById('quiz-start');
 const questionContainer = document.getElementById('question-container');
+const questionEl = document.getElementById('question');
+const answerBtnEl = document.getElementById('answer-container');
+
+let shuffledQuestions, currentQuestionIndex;
+
 // Set timer
 
 // Start button event listener
@@ -14,12 +29,44 @@ function startGame() {
   console.log('started');
   // hide start page
   startPage.classList.add('hide');
+  // shuffle questions
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
   // show questions
   questionContainer.classList.remove('hide');
+  // set next question
+  setNextQuestion();
 }
 
 // move to next question
-function setNextQuestion() {}
+function setNextQuestion() {
+  // reset questions
+  resetQuestion();
+  // show current question
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+// Populate question and answers in question container
+function showQuestion(question) {
+  questionEl.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener('click', selectAnswer);
+    answerBtnEl.appendChild(button);
+  });
+}
+
+// reset question
+function resetQuestion() {
+  while (answerBtnEl.firstChild) {
+    answerBtnEl.removeChild(answerBtnEl.firstChild);
+  }
+}
 
 // When the user selects an answer
 function selectAnswer() {}
